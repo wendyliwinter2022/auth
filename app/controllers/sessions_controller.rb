c928e80b3a9b1b7cc655985db5a7111a08212508
wendyliwinter2022 <wendy.li1@kellogg.northwestern.edu>
2022-03-12 07:23:27 +0000

    encrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,19 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = User.where ({email: params["email"]})[0]
+        @user = User.where({email: params["email"]})[0]
 
         if @user
-            if @user.password == params["password"]
+            #BCrypt::Password.create(params["user"]["password"])
+            if BCrypt::Password.new(@user.password) == params["password"]
+               flash[:notice] = "Logged in!"
                 redirect_to "/companies" 
             else    
+                flash[:notice] = "Wrong password"
                 redirect_to "/sessions/new"  
             end 
         else
+            flash[:notice] = "Wrong User"
             redirect_to "/sessions/new"  
         end
     end 
