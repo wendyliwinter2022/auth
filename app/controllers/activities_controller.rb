fbc9ffb3b2cd89e46c106c32df6114718e583af4
wendyliwinter2022 <wendy.li1@kellogg.northwestern.edu>
2022-03-13 02:55:10 +0000

    activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,6 +1,7 @@
 class ActivitiesController < ApplicationController
   def create
     @activity = Activity.new(params["activity"])
+    @activity.user_id = @current_user.id
     @activity.save
     redirect_to "/contacts/#{@activity.contact_id}"
   end
